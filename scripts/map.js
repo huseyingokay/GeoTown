8b5ef6339ec74c0185184b0ca01552c7e13b07ab
demirhansevim <72740003+demirhansevim@users.noreply.github.com>
2020-12-07 20:37:06 +0200

    map.js updated
    Functions added

---
+++ scripts/map.js
@@ -0,0 +1,36 @@
+function Map() {
+    this.svgImage = this.loadSVGMap();
+    this.isClickable = false;
+}
+
+Map.prototype.loadSVGMap = function() {
+    var connection = new XMLHttpRequest();
+    connection.onreadystatechange = readData;
+    connection.open("GET", "./assets/map.svg", true);
+    connection.send();
+    function readData() {
+        if (connection.readyState == 4) {
+            document.getElementById("map").innerHTML = this.responseText;
+        }
+    }
+}
+
+Map.prototype.reset = function() {
+    
+}
+
+Map.prototype.zoom = function (amount) {
+
+}
+
+Map.prototype.updateMap = function (districts) {
+
+}
+
+Map.prototype.setClickable = function () {
+
+}
+
+Map.prototype.onClick = function (e) {
+
+}
