f24d6eb9f4dd12f80192b6eac03cfdad8e50a749
demirhansevim <72740003+demirhansevim@users.noreply.github.com>
2020-12-17 11:06:40 +0200

    Districts updated, zoom added

--- scripts/map.js
+++ scripts/map.js
@@ -3,31 +3,51 @@ function Map() {
     this.isClickable = false;
 }
 
-Map.prototype.loadSVGMap = function() {
+Map.prototype.loadSVGMap = function () {
     var connection = new XMLHttpRequest();
     connection.onreadystatechange = readData;
     connection.open("GET", "./assets/map.svg", true);
     connection.send();
+    var outerThis = this;
     function readData() {
         if (connection.readyState == 4) {
             document.getElementById("map").innerHTML = this.responseText;
+
+            var beforePan = function (oldPan, newPan) {
+                var stopHorizontal = false,
+                    stopVertical = false,
+                    sizes = this.getSizes(),
+                    gutterWidth = sizes.width * 0.8,
+                    gutterHeight = sizes.height * 0.4,
+
+                    leftLimit = -((sizes.viewBox.x + sizes.viewBox.width) * sizes.realZoom) + gutterWidth,
+                    rightLimit = sizes.width - gutterWidth - (sizes.viewBox.x * sizes.realZoom),
+                    topLimit = -((sizes.viewBox.y + sizes.viewBox.height) * sizes.realZoom) + gutterHeight,
+                    bottomLimit = sizes.height - gutterHeight - (sizes.viewBox.y * sizes.realZoom);
+
+                customPan = {};
+                customPan.x = Math.max(leftLimit, Math.min(rightLimit, newPan.x));
+                customPan.y = Math.max(topLimit, Math.min(bottomLimit, newPan.y));
+
+                return customPan;
+            }
+
+            outerThis.panZoom = svgPanZoom('#map-svg', {
+                zoomScaleSensitivity: 0.3,
+                minZoom: 1,
+                maxZoom: 8,
+                dblClickZoomEnabled: false,
+                beforePan: beforePan
+            });
         }
     }
-}
-
-Map.prototype.reset = function() {
-    
-}
-
-Map.prototype.zoom = function (amount) {
-
 }
 
 Map.prototype.updateMap = function (districtName) {
     var districts = document.getElementsByClassName("district");
     var count = 0;
     for (var i = 0; i < districts.length; ++i) {
-        if (districts[i].id == districtName) {
+        if (!districts[i].classList.contains("highlighted") && districts[i].id.localeCompare(districtName, 'tr', { sensitivity: 'base' }) == 0) {
             districts[i].classList.add("highlighted");
             ++count;
         }
@@ -35,12 +55,16 @@ Map.prototype.updateMap = function (districtName) {
     return count != 0;
 }
 
-Map.prototype.setClickable = function () {
-
+Map.prototype.onClick = function () {
+    if (event.type == "mousedown")
+        this.isDragged = {x: event.clientX, y: event.clientY};
+    if (event.type == "mouseup")
+        this.isDragged = this.isDragged.x == event.clientX && this.isDragged.y == event.clientY;
+    if (Game.finished && event.type == "click" && this.isDragged) {
+        var district = TownDatabase.getDistrict(event.target.parentElement.parentElement.id, event.target.parentElement.id);
+        alert(`${district.parentCity.name} ${district.name} ${district.population} ${district.area}`);
+    }
 }
-
-Map.prototype.onClick = function (e) {
-
-}
+Map.prototype.onClick.isDragged = null;
 
 var TownMap = new Map();
